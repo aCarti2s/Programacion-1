dc69cde6a456c58b88e18cf57dc0e6d704ff677d
aCarti2s <acarti2ss@gmail.com>
2025-11-17 15:12:16 -0600

    Add files via upload

---
+++ suma_cuadradods_w.py
@@ -0,0 +1,7 @@
+n = int(input("Ingresa un numero entero: "))
+i=1
+suma=0
+while i<=n:
+     suma += i*1
+     i+=1
+print("La suma de los cuadrados es: ", suma)     
---
+++ suma_cuadrados_f.py
@@ -0,0 +1,5 @@
+n=int(input("Ingresa un numero entero n: "))
+suma=0
+for i in range(1, n+1):
+    suma+=i*1
+print("La suma de los cuadrados es:", suma)    
